a93296096719099b35ff4f7f3b7d22800ff09153
RenaOfficial <y.games@outlook.jp>
2023-04-14 06:59:27 +0900

    追加

---
+++ storage/framework/views/849d27cf6ebdcb1736d8c2bb6068ad9a.php
@@ -0,0 +1,15 @@
+<?php
+$apiUri = "https://api.gakerbot.net";
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <title>Discord Bot Client for WEB</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+
+</body>
+</html>
+<?php /**PATH /Users/Lunar_Runa/Desktop/projects/dct/www.gakerbot.net/resources/views/index.blade.php ENDPATH**/ ?>
