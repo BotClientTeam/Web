7464cf4da69a8f8d8978080b7b47c4ffd0ebaa10
Taka005 <01taka005@gmail.com>
2023-04-14 19:29:53 +0900

    修正

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 	session_start();
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="ja">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
