bd5135d272dd6360fff5a415bf5e10b764f3ed71
Taka005 <takanosumika.main@gmail.com>
2023-04-16 11:17:45 +0900

    修正

--- assets/js/TokenCheck.js
+++ assets/js/TokenCheck.js
@@ -1,13 +1,13 @@
 const LoginForm = document.getElementById("LoginForm");
 const TokenInput = document.getElementById("TokenInput");
 
-LoginForm.addEventListener("submit",async()=>{
-    if(
-      !TokenInput.value.match(/[0-9a-zA-Z_-]{24}\.[0-9a-zA-Z_-]{6}\.[0-9a-zA-Z_-]{38}/)||
-      !TokenInput.value.match(/[0-9a-zA-Z_-]{24}\.[0-9a-zA-Z_-]{6}\.[0-9a-zA-Z_-]{27}/)
-    ){
-      event.preventDefault();
-      TokenInput.value = "";
-      alert("BOTのTokenを入力してください");
-    }
+LoginForm.addEventListener("submit",async(event)=>{
+  if(
+    !TokenInput.value.match(/[0-9a-zA-Z_-]{24}\.[0-9a-zA-Z_-]{6}\.[0-9a-zA-Z_-]{38}/)||
+    !TokenInput.value.match(/[0-9a-zA-Z_-]{24}\.[0-9a-zA-Z_-]{6}\.[0-9a-zA-Z_-]{27}/)
+  ){
+    event.preventDefault();
+    TokenInput.value = "";
+    alert("BOTのTokenを入力してください");
+  }
 });
