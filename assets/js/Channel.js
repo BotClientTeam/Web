1a9619737a98a66d152a5eab0e093d81075d994b
Taka005 <01taka005@gmail.com>
2023-04-21 12:32:00 +0900

    スコープ修正

--- assets/js/Channel.js
+++ assets/js/Channel.js
@@ -1,37 +1,39 @@
-const ChannelList = document.querySelectorAll(".ChannelId");
-ChannelList.forEach(channel=>{
-  channel.addEventListener("click",async()=>{
-    const channelId = channel.dataset.itemId;
-    if(!channelId.match(/\d{18,19}/g)) return;
+(()=>{
+  const ChannelList = document.querySelectorAll(".ChannelId");
+  ChannelList.forEach(channel=>{
+    channel.addEventListener("click",async()=>{
+      const channelId = channel.dataset.itemId;
+      if(!channelId.match(/\d{18,19}/g)) return;
 
-    const Messages = await fetch(`../module/Message.php?channelId=${channelId}`,{
-      method: "GET",
-      credentials: "include"
-    })
-    .then(res=>res.text())
-    .catch(error=>{
-      console.error(error);
-      alert("メッセージを取得できませんでした");
+      const Messages = await fetch(`../module/Message.php?channelId=${channelId}`,{
+        method: "GET",
+        credentials: "include"
+      })
+      .then(res=>res.text())
+      .catch(error=>{
+        console.error(error);
+        alert("メッセージを取得できませんでした");
+      });
+
+      const ChannelName = await fetch(`../module/ChannelName.php?channelId=${channelId}`,{
+        method: "GET",
+        credentials: "include"
+      })
+      .then(res=>res.text())
+      .catch(error=>{
+        console.error(error);
+        alert("メッセージを取得できませんでした");
+      });
+
+      document.getElementById("messages").innerHTML = Messages;
+      document.getElementById("ChannelName").innerHTML = ChannelName;
+
+      ChannelList.forEach(channel=>{
+        channel.classList.remove("active");
+      });
+      document.getElementById(channelId).classList.add("active")
+
+      console.log("Loaded Message and ChannelName");
     });
-
-    const ChannelName = await fetch(`../module/ChannelName.php?channelId=${channelId}`,{
-      method: "GET",
-      credentials: "include"
-    })
-    .then(res=>res.text())
-    .catch(error=>{
-      console.error(error);
-      alert("メッセージを取得できませんでした");
-    });
-
-    document.getElementById("messages").innerHTML = Messages;
-    document.getElementById("ChannelName").innerHTML = ChannelName;
-
-    ChannelList.forEach(channel=>{
-      channel.classList.remove("active");
-    });
-    document.getElementById(channelId).classList.add("active")
-
-    console.log("Loaded Message and ChannelName");
   });
-});
+})();
